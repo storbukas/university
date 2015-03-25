8163684861ac7aeeacec6427f6245b8380e48ac8
Lars Erik Storbukås <larserik@virtualgeek.no>
2015-03-25 15:58:28 +0100

    partial commit of movie solution (not finished)

---
+++ INF237/set_5/movie.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * @author  Lars Erik Storbukås
+ * @mail    lst111@student.uib.no
+ * @date    25/03 - 2015
+ * @course  INF237 Algorithm Engineering
+ * @task    Movie - Set 5
+ *
+ **/
+
+int main() {
+    // start here
+    int nr_of_movies;
+    int nr_of_request;
+
+    scanf("%d%d", &nr_of_movies, &nr_of_request);
+    
+    // create array twice the size of movies
+    
+    // calculate tree structure
+    
+    // evaluate input and number of movies
+    // on top of 'choosen' movie
+
+    return 0;
+}
